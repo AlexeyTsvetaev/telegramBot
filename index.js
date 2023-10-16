99ba1f6862b05ff0ce897e1d20deb7790b6bf82c
Alexey Tsvetaev <hellsong649@gmail.com>
2023-10-16 16:14:50 +0300

    created commands /

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ const token = "6123820339:AAGxCwPlNFOFeZZyUB6HT3fEkX-p_5uyDnM"
 
 const bot = new TelegramApi(token, {polling: true})
 
-bot.on('message', msg => {
-  console.log(msg)
-})
+const start = () => {
+  bot.setMyCommands([
+    {command: "/start", description: "Начальное приветствие бота"},
+    {command: "/info", description: "Показать информацию о боте"},
+    {command: "/monitoring", description: "Показать список доступных шкафчиков"},
+    {command: "/openlock", description: "Открыть мой шкафчик"},
+  ])
+  
+  bot.on('message', msg => {
+    const text = msg.text
+    const chatId = msg.chat.id
+  
+    if (text === "/start") {
+      return bot.sendMessage(chatId, `Приветствую тебя ${msg.from.first_name}`)
+    }
+    console.log(msg)
+  })
+}
+
+start()
